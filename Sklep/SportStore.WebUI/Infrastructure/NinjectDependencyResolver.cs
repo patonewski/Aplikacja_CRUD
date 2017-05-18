8c493b0a0b6264511f446ff6fa77c4c0b12a2e58
patonewski <patonewski@o2.pl>
2017-05-18 18:18:19 +0200

    Dodanie stronicowania, TDD
    jw

--- Sklep/SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ Sklep/SportStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
-using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Abstract;
 namespace SportsStore.WebUI.Infrastructure
 {
     public class NinjectDependencyResolver : IDependencyResolver
@@ -26,13 +27,7 @@ namespace SportsStore.WebUI.Infrastructure
         }
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-            new Product { Name = "Piłka nożna", Price = 25 },
-            new Product { Name = "Deska surfingowa", Price = 179 },
-            new Product { Name = "Buty do biegania", Price = 95 }
-});
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
