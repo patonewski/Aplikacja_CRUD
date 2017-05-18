b77e2ca38ac5b6d79c3105f6a03291d5478b8a6b
patonewski <patonewski@o2.pl>
2017-05-18 18:19:57 +0200

    Testy jednostkowe łączy stron

--- Sklep/SportsStore.UnitTests/UnitTest1.cs
+++ Sklep/SportsStore.UnitTests/UnitTest1.cs
@@ -1,10 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 namespace SportsStore.UnitTests
 {
     [TestClass]
@@ -13,25 +17,29 @@ namespace SportsStore.UnitTests
         [TestMethod]
         public void Can_Paginate()
         {
-            // przygotowanie
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new Product[] {
-new Product {ProductID = 1, Name = "P1"},
-new Product {ProductID = 2, Name = "P2"},
-new Product {ProductID = 3, Name = "P3"},
-new Product {ProductID = 4, Name = "P4"},
-new Product {ProductID = 5, Name = "P5"}
-});
-            ProductController controller = new ProductController(mock.Object);
-            controller.PageSize = 3;
+            // …polecenie usunięte w celu zachowania zwięzłości…
+        }
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // przygotowanie — definiowanie metody pomocniczej HTML — potrzebujemy tego,
+            // aby użyć metody rozszerzającej
+            HtmlHelper myHelper = null;
+            // przygotowanie — tworzenie danych PagingInfo
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+            // przygotowanie — konfigurowanie delegatu z użyciem wyrażenia lambda
+            Func<int, string> pageUrlDelegate = i => "Strona" + i;
             // działanie
-            IEnumerable<Product> result =
-            (IEnumerable<Product>)controller.List(2).Model;
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
             // asercje
-            Product[] prodArray = result.ToArray();
-            Assert.IsTrue(prodArray.Length == 2);
-            Assert.AreEqual(prodArray[0].Name, "P4");
-            Assert.AreEqual(prodArray[1].Name, "P5");
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a>"
+            + @"<a class="" btn btn-default btn-primary selected"" href=""Strona2"">2</a>"
+            + @"<a class=""btn btn-default"" href=""Strona3"">3</a>", result.ToString());
         }
     }
 }
