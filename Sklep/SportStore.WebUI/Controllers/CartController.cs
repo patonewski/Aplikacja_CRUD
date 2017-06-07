6025e6263628950e4378ab87881d5f2e6549b33f
patonewski <patonewski@o2.pl>
2017-06-07 18:56:38 +0200

    Przesyłanie danych za pomocą POST

--- Sklep/SportStore.WebUI/Controllers/CartController.cs
+++ Sklep/SportStore.WebUI/Controllers/CartController.cs
@@ -8,9 +8,11 @@ namespace SportsStore.WebUI.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
-        public CartController(IProductRepository repo)
+        private IOrderProcessor orderProcessor;
+        public CartController(IProductRepository repo, IOrderProcessor proc)
         {
             repository = repo;
+            orderProcessor = proc;
         }
         public ViewResult Index(Cart cart, string returnUrl)
         {
@@ -47,6 +49,24 @@ namespace SportsStore.WebUI.Controllers
         public ViewResult Checkout()
         {
             return View(new ShippingDetails());
+        }
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Koszyk jest pusty!");
+            }
+            if (ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
